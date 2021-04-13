8baf24a5747f98351478f7810d6f3cc4fc789eab
susanshrestha28 <51448029+susanshrestha28@users.noreply.github.com>
2021-04-14 00:04:41 +0545

    Update index.js

--- index.js
+++ index.js
@@ -13,6 +13,15 @@ var marvel = api.createClient({
   publicKey: 'cbc5e466ee9ff1ee325a1c17b20c3868'
 , privateKey: 'ef8ab8438fd3941b085415a8f58a0060dd06a1fd'
 });
+
+app.get('/',(req,res) => {
+  marvel.characters.findAll()
+  .then(data => {
+    res.send(data);
+  })
+  .fail(console.error)
+  .done();
+})
 app.get('/characters',(req,res) => {
   marvel.characters.findAll()
   .then(data => {

975ad723b860d7237665e22177d35ef1646ef4cd
susanshrestha28 <51448029+susanshrestha28@users.noreply.github.com>
2021-04-14 00:27:20 +0545

    Update index.js

--- index.js
+++ index.js
@@ -1,44 +1,42 @@
 var app = require('express')();
 var cors = require('cors');
-//var api = require('marvel-api');
-//var bodyParser = require('body-parser')
+//const https = require('https')
+var api = require('marvel-api');
+var bodyParser = require('body-parser')
+app.use(cors({origin: '*'}));
 // parse application/x-www-form-urlencoded
-//app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }))
 
 // parse application/json
-//app.use(bodyParser.json())
-app.use(cors());
-// var marvel = api.createClient({
-//   publicKey: 'cbc5e466ee9ff1ee325a1c17b20c3868'
-// , privateKey: 'ef8ab8438fd3941b085415a8f58a0060dd06a1fd'
-// });
+app.use(bodyParser.json())
 
-app.get('/',(req,res) => {
-//   marvel.characters.findAll()
-//   .then(data => {
-//     res.send(data);
-//   })
-//   .fail(console.error)
-//   .done();
-  res.send(['hello'])
+var marvel = api.createClient({
+  publicKey: 'cbc5e466ee9ff1ee325a1c17b20c3868'
+, privateKey: 'ef8ab8438fd3941b085415a8f58a0060dd06a1fd'
+});
+marvel.characters.findAll()
+  .then(data => {
+    res.send(data);
+  })
+  .fail(console.error)
+  .done();
+app.get('/characters' , (req,res) => {
+  marvel.characters.findAll()
+  .then(data => {
+    res.send(data);
+  })
+  .fail(console.error)
+  .done();
 })
-// app.get('/characters',(req,res) => {
-//   marvel.characters.findAll()
-//   .then(data => {
-//     res.send(data);
-//   })
-//   .fail(console.error)
-//   .done();
-// })
-// app.post('/findbyname' , (req,res) => {
-//   console.log(req.body[0])
-//   marvel.characters.findNameStartsWith(req.body[0])
-//   .then(data => {
-//     res.send(data);
-//   })
-//   .fail(console.error)
-//   .done();
-// }) 
+app.post('/findbyname' , (req,res) => {
+  console.log(req.body[0])
+  marvel.characters.findNameStartsWith(req.body[0])
+  .then(data => {
+    res.send(data);
+  })
+  .fail(console.error)
+  .done();
+}) 
 app.listen(3000, () => {
     console.log('server started');
 })
